a338b29d72e4a7fc0303114d75ca251e121175c1
colin <163393924+officialcc@users.noreply.github.com>
2025-11-24 01:36:16 +0800

    Completed Lesson 300

--- internals/api/handlers/teachers.go
+++ internals/api/handlers/teachers.go
@@ -2,10 +2,14 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"grpcapi/internals/models"
 	"grpcapi/internals/repositories/mongodb"
+	"grpcapi/pkg/utils"
 	pb "grpcapi/proto/gen"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -41,4 +45,56 @@ func (s *Server) GetTeachers(ctx context.Context, req *pb.GetTeachersRequest) (*
 		return nil, status.Error(codes.Internal, err.Error())
 	}
 	return &pb.Teachers{Teachers: teachers}, nil
+}
+
+func (s *Server) UpdateTeachers(ctx context.Context, req *pb.Teachers) (*pb.Teachers, error) {
+	updatedTeachers, err := mongodb.ModifyTeachersInDb(ctx, req.Teachers)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &pb.Teachers{Teachers: updatedTeachers}, nil
+}
+
+func (s *Server) DeleteTeachers(ctx context.Context, req *pb.TeacherIds) (*pb.DeleteTeachersConfirmation, error) {
+	ids := req.GetIds()
+	var teacherIdsToDelete []string
+	for _, v := range ids {
+		teacherIdsToDelete = append(teacherIdsToDelete, v.Id)
+	}
+
+	client, err := mongodb.CreateMongoClient()
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "internal error")
+	}
+	defer client.Disconnect(ctx)
+
+	objectIds := make([]primitive.ObjectID, len(teacherIdsToDelete))
+	for i, id := range teacherIdsToDelete {
+		objectId, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			return nil, utils.ErrorHandler(err, fmt.Sprintf("incorrect id: %v", id))
+		}
+		objectIds[i] = objectId
+	}
+
+	filter := bson.M{"_id": bson.M{"$in": objectIds}}
+	result, err := client.Database("school").Collection("teachers").DeleteMany(ctx, filter)
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "internal error")
+	}
+
+	if result.DeletedCount == 0 {
+		return nil, utils.ErrorHandler(err, "no teachers were deleted. Ids/Entries do not exist.")
+	}
+
+	deletedIds := make([]string, result.DeletedCount)
+	for i, id := range objectIds {
+		deletedIds[i] = id.Hex()
+	}
+
+	return &pb.DeleteTeachersConfirmation{
+		Status: "Teachers successfully deleted",
+		DeletedIds: deletedIds,
+	}, nil
 }
--- internals/repositories/mongodb/teachers_crud.go
+++ internals/repositories/mongodb/teachers_crud.go
@@ -2,10 +2,11 @@ package mongodb
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"grpcapi/internals/models"
 	"grpcapi/pkg/utils"
 	pb "grpcapi/proto/gen"
-	"reflect"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -37,7 +38,7 @@ func AddTeachersToDb(ctx context.Context, teachersFromReq []*pb.Teacher) ([]*pb.
 			teacher.Id = objectId.Hex()
 		}
 
-		pbTeacher := mapModelTeacherToPb(teacher)
+		pbTeacher := mapModelTeacherToPb(*teacher)
 		addedTeachers = append(addedTeachers, pbTeacher)
 	}
 	return addedTeachers, nil
@@ -61,8 +62,8 @@ func GetTeachersFromDb(ctx context.Context, sortOptions bson.D, filter bson.M) (
 		return nil, utils.ErrorHandler(err, "Internal Error")
 	}
 	defer cursor.Close(ctx)
-	
-	teachers, err := decodeEntities(ctx, cursor, func() *pb.Teacher {return &pb.Teacher{}}, newModel)
+
+	teachers, err := decodeEntities(ctx, cursor, func() *pb.Teacher { return &pb.Teacher{} }, newModel)
 	if err != nil {
 		return nil, err
 	}
@@ -73,61 +74,50 @@ func newModel() *models.Teacher {
 	return &models.Teacher{}
 }
 
-// func decodeTeachers(ctx context.Context, cursor *mongo.Cursor) ([]*pb.Teacher, error) {
-// 	var teachers []*pb.Teacher
-// 	for cursor.Next(ctx) {
-// 		var teacher models.Teacher
-// 		err := cursor.Decode(&teacher)
-// 		if err != nil {
-// 			return nil, utils.ErrorHandler(err, "Internal Error")
-// 		}
-// 		pbTeacher := &pb.Teacher{}
-// 		modelVal := reflect.ValueOf(teacher)
-// 		pbVal := reflect.ValueOf(pbTeacher).Elem()
-
-// 		for i := 0; i < modelVal.NumField(); i++ {
-// 			modelField := modelVal.Field(i)
-// 			modelFieldName := modelField.Type().Field(i).Name
-
-// 			pbField := pbVal.FieldByName(modelFieldName)
-// 			if pbField.IsValid() && pbField.CanSet() {
-// 				pbField.Set(modelField)
-// 			}
-// 		}
-// 		teachers = append(teachers, pbTeacher)
-// 	}
-// 	return teachers, nil
-// }
-
-func mapModelTeacherToPb(teacher *models.Teacher) *pb.Teacher {
-	pbTeacher := &pb.Teacher{}
-	modelVal := reflect.ValueOf(*teacher)
-	pbVal := reflect.ValueOf(pbTeacher).Elem()
-
-	for i := 0; i < modelVal.NumField(); i++ {
-		modelField := modelVal.Field(i)
-		modelFieldType := modelVal.Type().Field(i)
-		pbField := pbVal.FieldByName(modelFieldType.Name)
-		if pbField.IsValid() && pbField.CanSet() {
-			pbField.Set(modelField)
-		}
+func ModifyTeachersInDb(ctx context.Context, pbTeachers []*pb.Teacher) ([]*pb.Teacher, error) {
+	client, err := CreateMongoClient()
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "internal error")
 	}
-	return pbTeacher
-}
 
-func mapPbTeacherToModelTeacher(pbTeacher *pb.Teacher) *models.Teacher {
-	modelTeacher := models.Teacher{}
-	pbVal := reflect.ValueOf(pbTeacher).Elem()
-	modelVal := reflect.ValueOf(&modelTeacher).Elem()
+	defer client.Disconnect(ctx)
 
-	for i := 0; i < pbVal.NumField(); i++ {
-		pbField := pbVal.Field(i)
-		fieldName := pbVal.Type().Field(i).Name
+	var updatedTeachers []*pb.Teacher
 
-		modelField := modelVal.FieldByName(fieldName)
-		if modelField.IsValid() && modelField.CanSet() {
-			modelField.Set(pbField)
+	for _, teacher := range pbTeachers {
+		if teacher.Id == "" {
+			return nil, utils.ErrorHandler(errors.New("id cannot be blank"), "ID cannot be blank")
 		}
+		modelTeacher := mapPbTeacherToModelTeacher(teacher)
+
+		objId, err := primitive.ObjectIDFromHex(teacher.Id)
+		if err != nil {
+			return nil, utils.ErrorHandler(err, "Invalid ID")
+		}
+
+		// Convert modelTeacher to BSON document
+		modelDoc, err := bson.Marshal(modelTeacher)
+		if err != nil {
+			return nil, utils.ErrorHandler(err, "internal error")
+		}
+
+		var updateDoc bson.M
+		err = bson.Unmarshal(modelDoc, &updateDoc)
+		if err != nil {
+			return nil, utils.ErrorHandler(err, "internal error")
+		}
+
+		// Remove  the _id field from the update document
+		delete(updateDoc, "_id")
+
+		_, err = client.Database("school").Collection("teachers").UpdateOne(ctx, bson.M{"_id": objId}, bson.M{"$set": updateDoc})
+		if err != nil {
+			return nil, utils.ErrorHandler(err, fmt.Sprintln("error updating teacher id:", teacher.Id))
+		}
+
+		updatedTeacher := mapModelTeacherToPb(*modelTeacher)
+
+		updatedTeachers = append(updatedTeachers, updatedTeacher)
 	}
-	return &modelTeacher
+	return updatedTeachers, nil
 }
